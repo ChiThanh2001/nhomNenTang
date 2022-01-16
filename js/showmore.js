8f2791630c5da4a23397c1ad3f1065e71f33190f
chithanh2001 <satthumaulanh2001@gmail.com>
2022-01-16 13:44:45 +0700

    hoan thien trang explore.html

---
+++ js/showmore.js
@@ -0,0 +1,7 @@
+var showmore = document.querySelector(".showmore")
+
+var center_col = document.querySelector(".my-center-col")
+
+showmore.onclick = function(e){
+    center_col.classList.toggle("showmore-content")
+}
